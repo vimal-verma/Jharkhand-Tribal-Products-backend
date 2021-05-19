7f731002d733421a87f4cde5368f52bbe8ac1d5f
vimalverma558 <kumarvimal558@gmail.com>
2021-05-19 11:29:17 +0530

    auth logic done 🚀

--- router/auth.js
+++ router/auth.js
@@ -1,7 +1,56 @@
 const route = require('express').Router()
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../model/user')
 
 route.get('/', (req, res) => {
     res.send("hello")
+})
+
+route.post('/register', async (req,res )=>{
+    const user = new User({
+        email : req.body.email,
+        password : req.body.password,
+        displayName : req.body.displayName,
+        imgurl : req.body.imgurl
+    })
+
+    if(user.password.length < 5){
+        return res.status(404).send('password length be greater than 4')
+    }
+
+    const finduser = await User.findOne({email : user.email})
+    if(finduser){
+        return res.status(404).send('email already registered')
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashpassword = await bcrypt.hash(user.password, salt)
+    user.password = hashpassword
+
+    try {
+        const saveduser = await user.save()
+        const accesstoken = jwt.sign({saveduser}, process.env.ACCESSTOKEN)
+        res.header('auth-token', accesstoken).send(accesstoken)
+    } catch (error) {
+        res.status(404).send(error)
+    }
+})
+
+route.post('/login' , async (req,res )=>{
+    const user = await User.findOne({email : req.body.email})
+
+    if(user){
+        const validpass = await bcrypt.compare(req.body.password, user.password)
+        if (validpass) {
+            const accesstoken = jwt.sign({user}, process.env.ACCESSTOKEN)
+            res.header('auth-token', accesstoken).send(accesstoken)
+        } else {
+            res.status(400).send('user credentials not match')
+        }
+    }else{
+        res.status(400).send('user not found, please register')
+    }
 })
 
 module.exports = route
